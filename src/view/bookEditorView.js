7a5a2e1d83a83eb1649dd24cfa7f2b3843fa31d3
Liao Yichen <2498273541@qq.com>
2022-03-13 18:00:18 +0800

    book editor

--- src/view/bookEditorView.js
+++ src/view/bookEditorView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button, Col, Container, Form, Row, InputGroup, FormControl} from 'react-bootstrap'
+import {Button, Col, Container, Form, Row, InputGroup, FormControl, Figure} from 'react-bootstrap'
 import PropTypes from 'prop-types';
 import SideBar from '../components/sideBar'
 import HeaderInfo from '../components/headerInfo'
@@ -10,13 +10,14 @@ export default class BookEditorView extends React.PureComponent {
   constructor (props) {
     super(props)
     this.state = {
-      img: null,
+      img: null, // TODO: img picker
       title: '',
       author: '',
       ISBN: '',
       price: 0,
       stock: 0,
       synopsis: '',
+      validated: false,
       isLoading: false,
     }
     autobind(this)
@@ -28,8 +29,18 @@ export default class BookEditorView extends React.PureComponent {
     }
   }
 
-  onConfirm () {
-    console.log(this.state)
+  handleSubmit (e) {
+    this.setState({validated: true});
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+      return;
+    }
+    // this.setState({isLoading: true});
+    // TODO: post request and set callback
+    // if success go back and refresh
+    // else set isLoading false and toast an alert
   }
 
   render () {
@@ -41,48 +52,76 @@ export default class BookEditorView extends React.PureComponent {
           <SideBar defaultActiveKey="/home"/>
         </Col>
         <Col sm={{ span: 5, offset: 2 }}>
-          <Form>
-            <InputGroup className="mb-3">
+          <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit}>
+            <InputGroup hasValidation className="mb-3">
               <InputGroup.Text>标题</InputGroup.Text>
               <FormControl
                 placeholder="Title"
                 value={this.state.title}
                 onChange={(e) => this.setState({title: e.target.value})}
+                required
               />
+              <Form.Control.Feedback type="invalid">
+                Please input a title.
+              </Form.Control.Feedback>
             </InputGroup>
-            <InputGroup className="mb-3">
+            <InputGroup hasValidation className="mb-3">
               <InputGroup.Text>作者</InputGroup.Text>
               <FormControl
                 placeholder="Author"
                 value={this.state.author}
                 onChange={(e) => this.setState({author: e.target.value})}
+                required
               />
+              <Form.Control.Feedback type="invalid">
+                Please input an author.
+              </Form.Control.Feedback>
             </InputGroup>
-            <InputGroup className="mb-3">
+            <InputGroup hasValidation className="mb-3">
               <InputGroup.Text>ISBN</InputGroup.Text>
               <FormControl
                 placeholder="ISBN"
                 value={this.state.ISBN}
                 onChange={(e) => this.setState({ISBN: e.target.value})}
+                required
               />
+              <Form.Control.Feedback type="invalid">
+                Please input ISBN.
+              </Form.Control.Feedback>
             </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>价格</InputGroup.Text>
-              <FormControl
-                type='number'
-                placeholder="Price"
-                value={this.state.price}
-                onChange={(e) => this.setState({price: e.target.value})}
-              />
-              <InputGroup.Text>库存</InputGroup.Text>
-              <FormControl
-                type='number'
-                placeholder="Stock"
-                value={this.state.stock}
-                onChange={(e) => this.setState({stock: e.target.value})}
-              />
-            </InputGroup>
-            <InputGroup>
+            <Row>
+              <Col>
+                <InputGroup hasValidation className="mb-3">
+                  <InputGroup.Text>价格</InputGroup.Text>
+                  <FormControl
+                    type='number'
+                    placeholder="Price"
+                    value={this.state.price}
+                    onChange={(e) => this.setState({price: isNaN(parseFloat(e.target.value)) ? '' : parseFloat(e.target.value)})}
+                    isInvalid={this.state.price === '' || this.state.price < 0}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please input a non-negative number.
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Col>
+              <Col>
+                <InputGroup hasValidation className="mb-3">
+                  <InputGroup.Text>库存</InputGroup.Text>
+                  <FormControl
+                    type='number'
+                    placeholder="Stock"
+                    value={this.state.stock}
+                    onChange={(e) => this.setState({stock: isNaN(parseInt(e.target.value)) ? '' : parseInt(e.target.value)})}
+                    isInvalid={this.state.stock === '' || this.state.stock < 0}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please input a non-negative integer.
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Col>
+            </Row>
+            <InputGroup hasValidation>
               <InputGroup.Text>简介</InputGroup.Text>
               <FormControl
                 as="textarea"
@@ -90,7 +129,11 @@ export default class BookEditorView extends React.PureComponent {
                 placeholder="Synopsis"
                 value={this.state.synopsis}
                 onChange={(e) => this.setState({synopsis: e.target.value})}
+                required
               />
+              <Form.Control.Feedback type="invalid">
+                Please input a synopsis.
+              </Form.Control.Feedback>
             </InputGroup>
             <br />
             <Form.Group as={Row} className="mb-3 justify-content-around">
@@ -98,8 +141,7 @@ export default class BookEditorView extends React.PureComponent {
                 <Button size="lg" className="w-100" variant="outline-success" onClick={() => history.back()}>取消</Button>
               </Col>
               <Col sm={3}>
-                <Button size="lg" className="w-100" variant="success" disabled={this.state.isLoading}
-                  onClick={!this.state.isLoading ? this.onConfirm : null}>{this.state.isLoading ? 'Loading…' : '确认'}</Button>
+                <Button size="lg" className="w-100" variant="success" type="submit" disabled={this.state.isLoading}>{this.state.isLoading ? 'Loading…' : '提交'}</Button>
               </Col>
             </Form.Group>
           </Form>
